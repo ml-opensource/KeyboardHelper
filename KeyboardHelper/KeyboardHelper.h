6c0d7fc12fe9c61be3d272185958572592e56424
Chris Combs <chco@nodes.dk>
2016-01-26 16:36:25 +0100

    Initial Commit

---
+++ KeyboardHelper/KeyboardHelper.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardHelper.h
+//  KeyboardHelper
+//
+//  Created by Chris Combs on 26/01/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardHelper.
+FOUNDATION_EXPORT double KeyboardHelperVersionNumber;
+
+//! Project version string for KeyboardHelper.
+FOUNDATION_EXPORT const unsigned char KeyboardHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardHelper/PublicHeader.h>
+
+
