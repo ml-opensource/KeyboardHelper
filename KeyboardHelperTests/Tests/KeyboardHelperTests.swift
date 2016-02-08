aa2ebea270dfc061947e002fa0e67caae8cd8ed7
Timmi Trinks <titr@nodes.dk>
2016-02-08 11:20:08 +0100

    - Swift version of KeyboardHelper - some tests

---
+++ KeyboardHelperTests/Tests/KeyboardHelperTests.swift
@@ -0,0 +1,14 @@
+//
+//  KeyboardHelperTests.swift
+//  KeyboardHelperTests
+//
+//  Created by Chris Combs on 26/01/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import XCTest
+@testable import KeyboardHelper
+
+class KeyboardHelperTests: XCTestCase {
+
+}
